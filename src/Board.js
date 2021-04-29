d7ce29a733b87bf4bb2d955a115772f2d9538825
Leslie Rodriguez <rleslie1015@gmail.com>
2021-04-28 22:12:25 -0500

    adds state for switching players turn

--- src/Board.js
+++ src/Board.js
@@ -2,12 +2,31 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
   }
 
+  renderSquare(i) {
+    return <Square
+    value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}
+     />;
+  }
+
+  handleClick = (i) => {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares : squares,
+      xIsNext: !this.state.xIsNext,
+    })
+  }
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + ( this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
