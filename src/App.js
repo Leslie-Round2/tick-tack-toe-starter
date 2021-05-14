e66345a40e1e464954772e7ea122d72dfa909a13
Leslie Rodriguez <rleslie1015@gmail.com>
2021-05-13 22:49:38 -0500

    added auth

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+import Game from './Game';
+function App() {
+return (
+<div>
+<h1>Hello from AWS Amplify</h1>
+<Game />,
+
+  <AmplifySignOut />
+</div>
+)
+}
+export default withAuthenticator(App)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
 import Game from './Game';
+import App from './App';
+
+Amplify.configure(config);
 
 ReactDOM.render(
-  <Game />,
+  <App />,
+  // <Game />,
   document.getElementById('root')
 );
